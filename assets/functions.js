188f85e6fe7499a8f344fa1c029300aa7afc9b54
alstat <alasaadstat@gmail.com>
2018-10-15 00:20:41 +0800

    updated

--- assets/functions.js
+++ assets/functions.js
@@ -19,3 +19,17 @@ function moreButton (buttonId, divId, label) {
         }, 700);
     }
 }
+
+function openCity(evt, cityName, content) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName(content);
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
